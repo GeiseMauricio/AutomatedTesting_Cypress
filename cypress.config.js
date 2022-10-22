7537c2b1a877af0b83e02194cc8bc93d595f51c7
GeiseMaurício <geise.mauricio@gmail.com>
2022-10-22 15:12:38 -0400

    adicionando informações do dashboard do cypress

--- cypress.config.js
+++ cypress.config.js
@@ -4,14 +4,13 @@ module.exports = defineConfig({
   
   
   e2e: {
+    projectId: "ru78k3",
     viewportWidth: 1000,
     viewportHeight: 660,
     baseUrl: 'https://buger-eats-qa.vercel.app',
+    
 
-    setupNodeEvents(on, config) {
-      // implement node event listeners here
-           
-    },
+    
    
   },
 
